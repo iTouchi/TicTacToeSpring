aa05079b506205d3470fb65d7cf1e5019199e57d
iTouchi <aytacsarkmaz@gmail.com>
2019-12-18 11:43:42 +0100

    commit

--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -1,8 +1,8 @@
 package domain;
 
-public abstract class Player { //interfaces maken
+public abstract class Player {
 
-
+// Hier een Interface van maken
     public abstract String getSymbol();
     public abstract void takeTurn();
     public abstract String getName();
